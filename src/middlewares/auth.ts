192132ce9a8e771f05c610e6795721d6c912add0
felipe <felipecacezi@gmail.com>
2023-09-18 07:46:48 -0300

    Inicio do estudo do basic auth

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from 'express'
+
+export const Auth = {
+    private: (req: Request, res:Response, next:NextFunction) => {
+        let success = false
+
+
+
+        if (success) {
+            next()
+        } else {
+            res.status(403)
+            .json({
+                error: 'Não autorizado'
+            })
+        }
+    }
+}
