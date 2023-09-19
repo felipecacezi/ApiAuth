3cd295d01d82e2c4e3deecadb4ebd489e208eb4c
felipe <felipecacezi@gmail.com>
2023-09-19 07:08:36 -0300

    finalizando estudo do basic auth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,10 +1,30 @@
 import { Request, Response, NextFunction } from 'express'
+import { User } from '../models/User';
 
 export const Auth = {
-    private: (req: Request, res:Response, next:NextFunction) => {
-        let success = false
+    private: async (req: Request, res:Response, next:NextFunction) => {
+        let success: boolean = false
+        const auth = req.headers.authorization ?? null
 
+        if (auth) {
+            const hash: string = auth.substring(6)
+            const decoded: string = Buffer.from(hash, 'base64').toString()
+            const data: string[] = decoded.split(':')
 
+            if (data.length === 2) {
+                const hasUser = await User.findOne(
+                    {
+                        where: {
+                            email: data[0],
+                            password: data[1]
+                        }
+                    }
+                );
+                if (hasUser) {
+                    success = true
+                }
+            }
+        }
 
         if (success) {
             next()
